adee612ebdd668db746f3d56a287ead2192f3bc1
Agustina Chaer <aguschaer@gmail.com>
2020-07-22 10:09:30 -0300

    fix addons on web

--- src/components/common/Box/stories.js
+++ src/components/common/Box/stories.js
@@ -4,6 +4,13 @@ import { storiesOf } from '@storybook/react-native';
 
 import Box from './index';
 
+export const task = {
+  id: '1',
+  title: 'Test Task',
+  state: 'TASK_INBOX',
+  updatedAt: new Date(2018, 0, 1, 9, 0),
+};
+
 export const defaultColor = () => <Box />;
 export const blueColor = () => <Box backgroundColor="#00F" />;
 
--- storybook/index.js
+++ storybook/index.js
@@ -2,6 +2,7 @@ import { AppRegistry } from 'react-native';
 import { getStorybookUI, configure } from '@storybook/react-native';
 
 import { loadStories } from './storyLoader';
+
 import './rn-addons';
 
 // import stories
